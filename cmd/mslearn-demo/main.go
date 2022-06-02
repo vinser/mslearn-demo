fc5e4744e91381fef87a44e91e88dd2ab88d93d7
Serguei Vine <vinser@outlook.com>
2022-06-02 09:58:34 +0500

    Say Hello

--- cmd/mslearn-demo/main.go
+++ cmd/mslearn-demo/main.go
@@ -6,9 +6,10 @@ var version, buildTime, target, goversion string
 
 func main() {
 	printHelp()
-}
-func printHelp() {
 	fmt.Println("Hello maker!")
+}
+
+func printHelp() {
 	fmt.Printf("Version: %s (%s)\n", version, target)
 	fmt.Printf("Build time: %s\n", buildTime)
 	fmt.Printf("Golang version: %s\n", goversion)
