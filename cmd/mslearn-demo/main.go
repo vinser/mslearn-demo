aa6de84c8b62836a319628edf171008495eae8ce
Serguei Vine <vinser@outlook.com>
2022-06-02 09:49:51 +0500

    New Feature - Help

--- cmd/mslearn-demo/main.go
+++ cmd/mslearn-demo/main.go
@@ -5,6 +5,9 @@ import "fmt"
 var version, buildTime, target, goversion string
 
 func main() {
+	printHelp()
+}
+func printHelp() {
 	fmt.Println("Hello maker!")
 	fmt.Printf("Version: %s (%s)\n", version, target)
 	fmt.Printf("Build time: %s\n", buildTime)
